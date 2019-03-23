587c3e872ee8106387b77e4386b321b21d73723c
th0r0nd0r <pjfritch1@gmail.com>
2019-03-23 16:52:36 -0700

    setup React skeleton + dev server

---
+++ client/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/home';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Home />, document.getElementById('main'));
+});
---
+++ client/components/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React is up and running!</h1>
+      </div>
+    );
+  }
+}
