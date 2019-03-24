df890571ebf7db3b4d5cf7669a4f15f782219f48
th0r0nd0r <pjfritch1@gmail.com>
2019-03-24 13:54:21 -0700

    move keys to keys.js

--- server/App.js
+++ server/App.js
@@ -4,13 +4,14 @@ import schema from './schema/schema';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth-routes';
 import path from 'path';
+import keys from './config/keys';
 
 const app = express();
 
 // connect to database
 // NOTE: the connection string should at the very least be an environment variable, oustide the application code.
 // I'm just exposing it here for the purpose of easily sharing access to the coding challenge.
-mongoose.connect('mongodb+srv://yew_nuser:p4ssw0rd@colonelclustered-pfwc5.mongodb.net/project?retryWrites=true', {useNewUrlParser: true});
+mongoose.connect(keys.mongoString, {useNewUrlParser: true});
 
 mongoose.connection.once('open', () => {
   console.log('the goose is in the burrow');
---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+// NOTE: these should be environment variables, oustide the version-controlled application code.
+// I'm just storing them here for the purpose of easily sharing access to the coding challenge.
+
+export default {
+  mongoString: 'mongodb+srv://yew_nuser:p4ssw0rd@colonelclustered-pfwc5.mongodb.net/project?retryWrites=true',
+};
